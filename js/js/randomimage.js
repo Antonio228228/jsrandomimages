e22fe45ace197774719665de1bf5cd3cc2ab2f74
twinterrors <70683889+twinterrors@users.noreply.github.com>
2020-12-03 10:56:29 +0200

    Add files via upload

---
+++ js/js/randomimage.js
@@ -0,0 +1,11 @@
+const mainimage = document.querySelector("#img");
+const button = document.querySelector(".btn");
+
+const images = ["img/pic1.jpg", "img/pic2.jpg", "img/pic3.jpg", "img/pic4.jpg"];
+
+button.addEventListener("click", ()=>{
+    console.log("hello from script file!")
+    let rnd = Math.floor(Math.random() * images.length);
+    mainimage.src = images[rnd];
+    console.log("Random number: ", rnd);
+});
